51a878363e2c5ebd9d7e80f60ca4ecc11ed4d178
labseven <adam.novotny55@gmail.com>
2017-04-22 11:35:15 -0400

    Release V1.0 (#1)
    * Add favicon
    
    * Add ability to delete text from wall
    
    * Add link on each element to delete it
    
    * Add css for mainpage
    
    * More css

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import pickle
 
 app = Flask(__name__)
+
+app.debug = True
+
 try:
     text_wall = pickle.load(open("wall.p", "rb"))
 except:
@@ -20,12 +23,31 @@ def login():
     else:
         return show_add_text_form()
 
+@app.route('/del_text', methods=['GET'])
+def del_text():
+    try:
+        remove_text_from_page(request.args['del_index'])
+    except:
+        pass
+
+    return redirect('/')
+
+@app.route('/test_post')
+def test_post():
+    return render_template('test_post_button.html')
+
 def add_text_to_page(in_text):
-    text_wall.append(in_text)
-    pickle.dump(text_wall, open("wall.p", "wb"))
+    if in_text:
+        text_wall.append(in_text)
+        pickle.dump(text_wall, open("wall.p", "wb"))
 
 def show_add_text_form():
     return render_template('add_text.html')
 
+def remove_text_from_page(index):
+    del text_wall[int(index)]
+    pickle.dump(text_wall, open("wall.p", "wb"))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0")
